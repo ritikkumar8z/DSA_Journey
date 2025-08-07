69745948ae08a09425d4f8d8814de8dd0c3d10b4
RiTiK <168961238+ritikkumar8z@users.noreply.github.com>
2025-08-07 23:53:15 +0530

    Update 043_Ford-Fulkerson.py

--- 043_Ford-Fulkerson.py
+++ 043_Ford-Fulkerson.py
@@ -2,7 +2,7 @@
 
 class Graph:
     def __init__(self, size):
-        self.adj_matrix = [[0] * size for _ in range(size)]
+        self.adj_matrix = [[0] * size for _ in range(size)] 
         self.size = size
         self.vertex_data = [''] * size 
  
