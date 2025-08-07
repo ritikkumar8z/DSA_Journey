5c209ab042152b6a3d3e0a848656fee61092ec71
RiTiK <168961238+ritikkumar8z@users.noreply.github.com>
2025-08-07 23:52:01 +0530

    Update 043_Ford-Fulkerson.py

--- 043_Ford-Fulkerson.py
+++ 043_Ford-Fulkerson.py
@@ -4,7 +4,7 @@ class Graph:
     def __init__(self, size):
         self.adj_matrix = [[0] * size for _ in range(size)]
         self.size = size
-        self.vertex_data = [''] * size
+        self.vertex_data = [''] * size 
  
     def add_edge(self, u, v, c): 
         self.adj_matrix[u][v] = c 
