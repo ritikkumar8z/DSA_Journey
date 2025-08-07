95730f87fa199e3e5df5cb98387cf903b2d7518a
RiTiK <168961238+ritikkumar8z@users.noreply.github.com>
2025-08-07 23:51:32 +0530

    Update 043_Ford-Fulkerson.py

--- 043_Ford-Fulkerson.py
+++ 043_Ford-Fulkerson.py
@@ -7,7 +7,7 @@ class Graph:
         self.vertex_data = [''] * size
  
     def add_edge(self, u, v, c): 
-        self.adj_matrix[u][v] = c
+        self.adj_matrix[u][v] = c 
 
     def add_vertex_data(self, vertex, data):
         if 0 <= vertex < self.size:
